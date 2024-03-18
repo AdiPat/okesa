b407dd36dfdea0b460224212e36947645474afbf
Aditya Patange <contact.adityapatange@gmail.com>
2024-03-18 18:42:16 +0530

    makakasho/pos: use json schema and gpt-4

--- src/core/makakasho.ts
+++ src/core/makakasho.ts
@@ -22,7 +22,7 @@ class Makakasho {
           content: prompt,
         },
       ],
-      model: "gpt-3.5-turbo",
+      model: "gpt-4",
     });
 
     const resultContent = result.choices[0].message.content;
--- src/core/pos-tagger.ts
+++ src/core/pos-tagger.ts
@@ -1,13 +1,27 @@
 import { LLMRunner } from ".";
 
-const PosTaggingJSONSchema = [
-  { word: "string", pos: "the identified part of speech" },
-];
+const PosTaggingJSONSchema = {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      word: {
+        type: "string",
+      },
+      pos: {
+        type: "string",
+      },
+    },
+    required: ["word", "pos"],
+  },
+};
 
 async function posTag(brainRunner: LLMRunner, text: string) {
   const system = "You are a NLP (Natural Language Processing) Tokenizer";
 
   const prompt = `Perform Part Of Speech (POS) tagging on the given text. Return the result in JSON as per the given schema.\
+  Only return a valid JSON.\
   1. JSON Schema:
   ${JSON.stringify(PosTaggingJSONSchema)}
   2. Text:
